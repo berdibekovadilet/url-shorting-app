3aa840dcfebc4e9ec308f8d081376bfdc928a1d7
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-25 13:35:35 +0600

    create linkSlice

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,43 @@
+import { useForm } from "react-hook-form";
 import styles from "./Form.module.scss";
 
 const Form = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    // reset,
+  } = useForm({
+    mode: "onSubmit",
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.input} tabindex="0">
-        <input type="text" placeholder="Введите ссылку для сокращения" />
-        <button>Сократить</button>
-      </div>
+      <form
+        className={styles.form}
+        tabIndex="0"
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          type="url"
+          placeholder="Введите ссылку для сокращения"
+          {...register("Url", {
+            required: "Пожалуйста введите ссылку",
+            pattern: {
+              value:
+                /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g,
+              message: "Неправильная ссылка",
+            },
+          })}
+        />
+        {errors.Url && <div className={styles.error}>{errors.Url.message}</div>}
+        <button type="submit">Сократить</button>
+      </form>
     </div>
   );
 };
---
+++ src/store/slice/linkSlice.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_BASE_URL } from "../../config";
+
+export const createShortLink = createAsyncThunk(
+  "links/createShortLink",
+  async (url) => {
+    const response = await fetch(API_BASE_URL + url, { method: "POST" });
+    return await response.json();
+  }
+);
+
+const initialState = {
+  items: [],
+  loading: "idle",
+};
+
+const linkSlice = createSlice({
+  name: "links",
+  initialState: initialState,
+  //   reducers: {},
+  extraReducers: {
+    [createShortLink.rejected]: (state) => {
+      state.loading = "rejected";
+    },
+    [createShortLink.pending]: (state) => {
+      state.loading = "loading";
+    },
+    [createShortLink.fulfilled]: (state, action) => {
+      const { ok, result } = action.payload;
+      if (ok) {
+        state.items.push(result);
+        state.loading = "idle";
+      } else {
+        state.loading = "error";
+      }
+    },
+  },
+});
+
+export default linkSlice.reducer;
