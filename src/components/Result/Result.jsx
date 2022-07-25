aa642ec828ffaa0f463b9c4f2c6d73bd1810b8f9
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-25 11:22:45 +0600

    fix Result component

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -5,7 +5,8 @@ const Result = () => {
     <div className={styles.container}>
       <h2>Готово</h2>
       <div className={styles.wrapper}>
-        <p>https://inlnk.ru/Rj5VwA</p>
+        <p>https://berdibekovadilet.com/</p>
+        <p className={styles.newlink}>https://inlnk.ru/Rj5VwA</p>
         <button>Скопировать</button>
       </div>
     </div>
