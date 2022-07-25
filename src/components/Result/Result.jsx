87a3ee1b3f62545128bb55e75761658f33a27b63
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-25 15:23:54 +0600

    added animation

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,6 +1,7 @@
 import styles from "./Result.module.scss";
 import { useSelector } from "react-redux";
 import { selectLinks } from "../../store/slice/linkSlice";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Result = () => {
   const links = useSelector(selectLinks);
@@ -10,11 +11,23 @@ const Result = () => {
     <div className={styles.container}>
       <h2>Готово</h2>
       {links.map((item) => (
-        <div className={styles.wrapper} key={item.code}>
-          <span>{item.original_link}</span>
-          <span className={styles.newlink}>{item.full_short_link2}</span>
-          <button>Скопировать</button>
-        </div>
+        <AnimatePresence key={item.code}>
+          <motion.div
+            className={styles.item}
+            initial={{
+              opacity: 0,
+              height: 0,
+            }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+            }}
+          >
+            <span>{item.original_link}</span>
+            <span className={styles.newlink}>{item.full_short_link2}</span>
+            <button>Скопировать</button>
+          </motion.div>
+        </AnimatePresence>
       ))}
     </div>
   );
